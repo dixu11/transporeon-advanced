f605c36320411e391bdbf6572f5340033acdecbc
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-04-28 11:56:00 +0200

    details about interfaces

--- src/main/java/basics/inheritance/animals/Wild.java
+++ src/main/java/basics/inheritance/animals/Wild.java
@@ -1,5 +1,15 @@
 package basics.inheritance.animals;
 
 public interface Wild {
-    void attack();
+
+   public final static int NUMBER = 10; //all fields in interfaces are public static final by definition
+
+    default void attack(){ //default means it is no longer abstract
+        System.out.println("It attacks!");
+    }
+
+    // we can make static methods like in every other classes
+    public static void main(String[] args) {
+
+    }
 }
