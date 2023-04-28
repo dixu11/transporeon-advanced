1b658921874a0f654a1f3cf6a35fdd1a76f03199
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-04-28 13:04:59 +0200

    challange

---
+++ src/main/java/basics/exceptions/Main2.java
@@ -0,0 +1,21 @@
+package basics.exceptions;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        //call to method
+        int value = readNumber();
+        System.out.println("result is: " + value);
+
+
+    }
+
+    public static int readNumber() {
+        //create scanner, ask for number
+        //if user types text, ask for number again
+        //return users number
+        return 0;
+    }
+
+}
