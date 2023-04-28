53eab73b17dfd9b5c0e309592106cd71c481b08f
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-04-28 13:21:23 +0200

    recursive solution for the challenge

--- src/main/java/basics/exceptions/Main2.java
+++ src/main/java/basics/exceptions/Main2.java
@@ -1,5 +1,6 @@
 package basics.exceptions;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main2 {
@@ -7,15 +8,18 @@ public class Main2 {
         //call to method
         int value = readNumber();
         System.out.println("result is: " + value);
-
-
     }
 
     public static int readNumber() {
-        //create scanner, ask for number
-        //if user types text, ask for number again
-        //return users number
-        return 0;
+        Scanner scanner = new Scanner(System.in);
+        int result = 0;
+        try {
+            System.out.println("Give me number");
+            result = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            result = readNumber();
+        }
+        return result;
     }
 
 }
