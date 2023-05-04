effebf86c256f60ea1050c9e0fc6e7baef802ab6
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-05-04 13:07:22 +0200

    different types of classes in java

---
+++ src/main/java/oop/classes/ProductNotForSale.java
@@ -0,0 +1,13 @@
+package oop.classes;
+
+public class ProductNotForSale  extends Product{
+    public ProductNotForSale(String name) {
+        super(name);
+    }
+
+    @Override
+    public void show() {
+        System.out.println("This is product with name " + name);
+        System.out.println("Sorry, this product is not for sale :(");
+    }
+}
