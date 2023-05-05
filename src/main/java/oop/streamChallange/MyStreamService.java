2c3a42b0b43dc6dae0fae05ab7d22998d690881d
Daniel Szlicht <daniel.szlicht25@gmail.com>
2023-05-05 12:22:55 +0200

    streams

---
+++ src/main/java/oop/streamChallange/MyStreamService.java
@@ -0,0 +1,61 @@
+package oop.streamChallange;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MyStreamService implements StreamService{
+    @Override
+    public void sortAndPrint(List<String> names) {
+    }
+
+    @Override
+    public int distinctAndCountNumbers(int[] numbers) {
+
+        return 0;
+    }
+
+    @Override
+    public List<String> computeMaleNames(List<String> names) {
+        return null;
+    }
+
+    @Override
+    public void printNumbersOfRange(int[] numbers, int minValue, int maxValue) {
+
+    }
+
+    @Override
+    public List<Integer> computeNamesLength(List<String> names) {
+        return null;
+    }
+
+    @Override
+    public List<Person> buildPeopleWithNames(List<String> names) {
+        return null;
+    }
+
+    @Override
+    public List<Person> findPeopleOfIdGreaterThan(List<Person> people, int id) {
+        return null;
+    }
+
+    @Override
+    public boolean hasSenior(List<Person> people) {
+        return false;
+    }
+
+    @Override
+    public double sumTotalCash(List<Person> people) {
+        return 0;
+    }
+
+    @Override
+    public Person findRichestPerson(List<Person> people) {
+        return null;
+    }
+
+    @Override
+    public double computeAverageAge(List<Person> people) {
+        return 0;
+    }
+}
